325d3ffe3ff26618f5e952f7f44fc8cccc0aca0d
VanThanh <thanhnguyen@gkxim.com>
2022-01-23 14:22:44 +0700

    update react-native-gesture-handler 2.2.0, update README

--- template/index.js
+++ template/index.js
@@ -2,10 +2,13 @@
  * @format
  */
 import 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
+LogBox.ignoreLogs([
+    "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
+]);
 console.log = () => { };
 console.warn = () => { };
 console.error = () => { };
