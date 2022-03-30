89f7a87174c5d569ba45577cbc1ce816d628eec5
VanThanh <thanhnguyen@gkxim.com>
2022-03-30 14:57:02 +0700

    custom text component

---
+++ template/src/components/text/Text.js
@@ -0,0 +1,24 @@
+import { Text as TextRN } from 'react-native'
+import React from 'react'
+
+function TextComponent(props) {
+
+    const {
+        children,
+        style = {},
+        ...rest
+    } = props;
+
+    return (
+        <TextRN
+            allowFontScaling={false}
+            {...rest}
+            style={{ color: "rgb(28, 28, 30)", ...style }}>
+
+            {children}
+
+        </TextRN>
+    )
+}
+
+export const Text = TextComponent; // || memo(TextComponent, equals);
--- template/src/components/text/index.js
+++ template/src/components/text/index.js
@@ -1,1 +1,2 @@
 export * from './TextTranslate'
+export * from './Text'
