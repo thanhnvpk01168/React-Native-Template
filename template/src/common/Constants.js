044ae2d17ad07bec6abf3682b54e823aa7a4e5d1
VanThanh <thanhnguyen@gkxim.com>
2022-08-02 17:03:08 +0700

    update to RN 0.69.3

--- template/src/common/Constants.js
+++ template/src/common/Constants.js
@@ -1,25 +1,25 @@
 import { Dimensions, Platform } from "react-native";
-import { getWindowHeightAndroid } from "./nativeModule";
+import { initialWindowMetrics } from "react-native-safe-area-context";
 
 export const isIos = Platform.OS === 'ios';
 
 const { width, height } = Dimensions.get("window");
 export const { deviceW, deviceH } = {
     deviceW: width,
-    deviceH: isIos ? height : getWindowHeightAndroid()
+    deviceH: isIos ? height : initialWindowMetrics.frame.height + initialWindowMetrics.frame.y
 }
 
 
-export const COLORS ={
-    warnBg:'#fff3cd',
-    warnText:'#664d03',
+export const COLORS = {
+    warnBg: '#fff3cd',
+    warnText: '#664d03',
 
-    successBg:'#d1e7dd',
-    successText:'#0f5132',
+    successBg: '#d1e7dd',
+    successText: '#0f5132',
 
-    errorBg:'#f8d7da',
-    errorText:'#842029',
+    errorBg: '#f8d7da',
+    errorText: '#842029',
 
-    infoBg:'#cff4fc',
-    infoText:'#055160',
+    infoBg: '#cff4fc',
+    infoText: '#055160',
 }
--- template/src/common/hooks/Hooks.js
+++ template/src/common/hooks/Hooks.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import isEqual from "react-fast-compare";
 import { Dimensions } from "react-native";
+import { initialWindowMetrics } from "react-native-safe-area-context";
 import { useSelector as useReduxSelector } from 'react-redux';
 
 import { deviceH, deviceW, isIos } from "../Constants";
-import { getWindowHeightAndroid } from "../nativeModule";
 
 export function useWindowDimensions() {
     const [windowDimensions, setWindowDimensions] = useState({ w: deviceW, h: deviceH });
@@ -14,7 +14,7 @@ export function useWindowDimensions() {
             let w = Dimensions.get("window").width;
             let h = Dimensions.get("window").height;
             if (!isIos && width < height) {
-                h = getWindowHeightAndroid();
+                h = initialWindowMetrics.frame.height + initialWindowMetrics.frame.y
             }
             setWindowDimensions({ w, h })
         })
